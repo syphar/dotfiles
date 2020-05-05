eb586651e89dc351622039eca5870addb22c0741
Denis Cornehl <denis.cornehl@gmail.com>
2020-05-05 09:24:22 +0200

    drop unnecessary script (tmate show-messages works )

--- bin/tmate-info.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# https://github.com/tmate-io/tmate/issues/86
-tmate -S $1 display -p '#{tmate_ssh}'    # Prints the SSH connection string
-tmate -S $1 display -p '#{tmate_ssh_ro}' # Prints the read-only SSH connection string
-tmate -S $1 display -p '#{tmate_web}'    # Prints the web connection string
-tmate -S $1 display -p '#{tmate_web_ro}' # Prints the read-only web connection string
