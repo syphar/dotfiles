af644a01c3e114d5494c50868c6af505428e8546
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-31 14:26:11 +0100

    project list caching
    https://gist.github.com/akorn/51ee2fe7d36fa139723c851d87e56096

--- find_projects.sh
+++ find_projects.sh
@@ -5,10 +5,12 @@ src_dir="$HOME/src"
 
 echo "$HOME/.config/nvim/"
 
-# (
+(
     # all Cargo projects including sub-crates
-    fd Cargo.toml "$src_dir" --exec-batch printf "%s\n" \{//\}/ &
+    $HOME/bin/runcached --ignore-env --ttl 86400 \
+        fd Cargo.toml "$src_dir" --exec-batch printf "%s\n" \{//\}/
 
     # all git repos, a little slow
-    $HOME/src/dotfiles/find_repos.sh "$src_dir" &
-# ) | sort | uniq
+    $HOME/bin/runcached --ignore-env --ttl 86400 \
+        $HOME/src/dotfiles/find_repos.sh "$src_dir"
+) | sort | uniq
