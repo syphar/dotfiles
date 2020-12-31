6fbd13589f40ac54a7852210eabc3401a338b752
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-31 14:05:46 +0100

    find-projects and repos , more optimization

--- find_projects.sh
+++ find_projects.sh
@@ -5,13 +5,10 @@ src_dir="$HOME/src"
 
 echo "$HOME/.config/nvim/"
 
-(
-    # starting in src_dir
-    cd "$src_dir"
-
+# (
     # all Cargo projects including sub-crates
-    fd Cargo.toml --exec echo $src_dir/\{//\}/ &
+    fd Cargo.toml "$src_dir" --exec-batch printf "%s\n" \{//\}/ &
 
     # all git repos, a little slow
     $HOME/src/dotfiles/find_repos.sh "$src_dir" &
-)
+# ) | sort | uniq
--- find_repos.sh
+++ find_repos.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-(
-    cd $1 &&
-    fd \
-        --type d \
-        --exclude "target" \
-        --exclude "node_modules" \
-        --exclude ".direnv" \
-        --exclude "venv" \
-        --exclude "venv2" \
-        --prune \
-        --no-ignore \
-        --hidden "^\.git$" \
-        --exec echo $1/\{//\}/
-)
+fd \
+    --type d \
+    --exclude "target" \
+    --exclude "node_modules" \
+    --exclude ".direnv" \
+    --exclude "venv" \
+    --exclude "venv2" \
+    --prune \
+    --no-ignore \
+    --hidden \
+    "^\.git$" \
+    "$1" \
+    --exec-batch printf "%s\n" \{//\}/
