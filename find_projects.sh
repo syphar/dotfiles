e97f6c51bdf5af01ba81ea74f83907dec1d7235e
Denis Cornehl <denis.cornehl@gmail.com>
2020-12-31 13:38:43 +0100

    [zsh] project-open, run find scripts in parallel

--- find_projects.sh
+++ find_projects.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-src_dir="$HOME/src/"
+src_dir="$HOME/src"
 
 echo "$HOME/.config/nvim/"
 
 (
     # starting in src_dir
-    cd "$src_dir" &&
-    # all Cargo projects
-    fd Cargo.toml --exec echo $src_dir\{//\}/ &&
+    cd "$src_dir"
+
+    # all Cargo projects including sub-crates
+    fd Cargo.toml --exec echo $src_dir/\{//\}/ &
 
     # all git repos, a little slow
-    $HOME/src/dotfiles/find_repos.sh "$src_dir"
+    $HOME/src/dotfiles/find_repos.sh "$src_dir" &
 )
