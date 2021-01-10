9368b139a2bd72462f52032a9238b0bbed7d075b
Denis Cornehl <denis.cornehl@gmail.com>
2021-01-10 22:42:19 +0100

    [tasks] delete more

--- clear_disk_space.sh
+++ clear_disk_space.sh
@@ -6,8 +6,10 @@ SRC_DIR="$HOME/src"
 echo "remove all pyenv versions"
 rm -rf ~/.pyenv/versions/*
 
-echo "delete all .direnv directories"
+echo "delete all .direnv, .tox or node_modules directories"
 fd --type d --no-ignore --hidden "^\.direnv$" "$SRC_DIR" --exec rm -rf {}
+fd --type d --no-ignore --hidden "^\.tox$" "$SRC_DIR" --exec rm -rf {}
+fd --type d --no-ignore --prune node_modules "$SRC_DIR" --exec rm -rf {}
 
 echo "delete caches"
 rm -rf ~/Library/Caches/*
@@ -25,6 +27,10 @@ docker volume prune -f
 
 echo "clear rust target directories"
 fd Cargo.toml "$SRC_DIR" --exec rm -rf \{//\}/target
+rm -rf ~/src/rust-lang/docs.rs/.rustwide-docker
 
 # clear thermondo backups
 rm -rf "$SRC_DIR/thermondo/backend/sql/backups"
+
+# delete some logs
+rm -f /usr/local/var/log/postgres.log
