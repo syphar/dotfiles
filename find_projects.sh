daac6d8c6ba086687dc45402572ddcd6c85cc28c
Denis Cornehl <denis.cornehl@gmail.com>
2021-03-13 09:03:56 +0100

    add go projects to automatic project list

--- find_projects.sh
+++ find_projects.sh
@@ -10,6 +10,10 @@ echo "$HOME/.config/nvim/"
     $HOME/bin/runcached \
         fd Cargo.toml "$src_dir" --exec-batch printf "%s\n" \{//\}/
 
+    # all Go projects
+    $HOME/bin/runcached \
+        fd go.mod "$src_dir" --exec-batch printf "%s\n" \{//\}/
+
     # all git repos, a little slow
     $HOME/bin/runcached \
         $HOME/src/dotfiles/find_repos.sh "$src_dir"
