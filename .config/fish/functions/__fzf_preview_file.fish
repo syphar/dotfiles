1ddd3003442c5d5a726c685d35c9d90c8e2f1a1d
Denis Cornehl <denis@cornehl.org>
2021-05-11 11:28:30 +0200

    fisher update

--- .config/fish/functions/__fzf_preview_file.fish
+++ .config/fish/functions/__fzf_preview_file.fish
@@ -4,18 +4,7 @@ function __fzf_preview_file --description "Print a preview for the given file ba
     # as one argument, we collect all the arguments into one single variable and treat that as the path
     set file_path $argv
 
-    if test -f "$file_path" # regular file
-        bat --style=numbers --color=always "$file_path"
-    else if test -d "$file_path" # directory
-        if set --query fzf_preview_dir_cmd
-            # need to escape quotes to make sure eval receives file_path as a single arg
-            eval $fzf_preview_dir_cmd \"$file_path\"
-        else
-            # -A list hidden files as well, except for . and ..
-            # -F helps classify files by appending symbols after the file name
-            command ls -A -F "$file_path"
-        end
-    else if test -L "$file_path" # symlink
+    if test -L "$file_path" # symlink
         # notify user and recurse on the target of the symlink, which can be any of these file types
         set -l target_path (realpath $file_path)
 
@@ -24,6 +13,22 @@ function __fzf_preview_file --description "Print a preview for the given file ba
         set_color normal
 
         __fzf_preview_file "$target_path"
+    else if test -f "$file_path" # regular file
+        if set --query fzf_preview_file_cmd
+            # need to escape quotes to make sure eval receives file_path as a single arg
+            eval $fzf_preview_file_cmd \"$file_path\"
+        else
+            bat --style=numbers --color=always "$file_path"
+        end
+    else if test -d "$file_path" # directory
+        if set --query fzf_preview_dir_cmd
+            # see above
+            eval $fzf_preview_dir_cmd \"$file_path\"
+        else
+            # -A list hidden files as well, except for . and ..
+            # -F helps classify files by appending symbols after the file name
+            command ls -A -F "$file_path"
+        end
     else if test -c "$file_path"
         __fzf_report_file_type "$file_path" "character device file"
     else if test -b "$file_path"
