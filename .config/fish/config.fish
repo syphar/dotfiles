d73e1b27b3345749dec74f6e16b4f193f024c0a1
Denis Cornehl <denis@cornehl.org>
2021-05-17 08:06:24 +0200

    [fish] potential pyenv changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,8 +3,11 @@ source $XDG_CONFIG_HOME/fish/key_bindings.fish
 source $XDG_CONFIG_HOME/fish/environment.fish
 
 set --universal tide_pwd_truncate_margin 999
-set --universal -x tide_right_prompt_items status cmd_duration context jobs heroku virtual_env
+set --universal tide_right_prompt_items status cmd_duration context jobs heroku virtual_env
 
 direnv hook fish | source
+
+# status is-login; and pyenv init --path | source
+# pyenv init - | source
 
 # vim: et ts=2 sts=2 sw=2
