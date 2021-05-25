37bd658793644c11ff7eabf5e6e7157846a1ec78
Denis Cornehl <denis@cornehl.org>
2021-05-25 09:08:06 +0200

    [fish] small code improvemnets

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,8 @@ source $XDG_CONFIG_HOME/fish/aliases.fish
 source $XDG_CONFIG_HOME/fish/key_bindings.fish
 source $XDG_CONFIG_HOME/fish/environment.fish
 
-set --universal tide_pwd_truncate_margin 999
-set --universal tide_right_prompt_items status cmd_duration context jobs heroku virtual_env
+set --global tide_pwd_truncate_margin 999
+set --global tide_right_prompt_items status cmd_duration context jobs heroku virtual_env
 
 direnv hook fish | source
 
--- .config/fish/functions/vv.fish
+++ .config/fish/functions/vv.fish
@@ -1,12 +1,20 @@
 # open a file from this projects with vim
 function vv
-  set fn (
+  set filename (
     fd --color=always --type f --type l --hidden --follow --exclude .git 2>/dev/null \
     | fzf
   )
   if test $status -eq 0
-      eval $EDITOR $fn
+      eval $EDITOR $filename
   end
 
   commandline -f repaint
 end
+
+
+# old zsh command
+# local file
+# file=$(
+#   eval "$FZF_DEFAULT_COMMAND" |
+#   fzf
+# ) && $EDITOR $file
