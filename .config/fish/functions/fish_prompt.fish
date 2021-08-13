2f1d3935f8690b942e9cae4b48ea684c38aef5e6
Denis Cornehl <denis@cornehl.org>
2021-08-13 07:00:30 +0200

    fisher update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -14,26 +14,19 @@ function _tide_refresh_prompt --on-variable $_tide_left_prompt_display_var --on-
 end
 
 function fish_prompt
-    set -lx _tide_last_status $status
-    set -lx _tide_last_pipestatus $pipestatus
-
-    if not set -e _tide_self_repainting
-        _tide_jobs_number=(jobs --pid | count) fish --command "
-            set -g CMD_DURATION $CMD_DURATION
-            set -g COLUMNS $COLUMNS
-            set -g fish_bind_mode $fish_bind_mode
-            set -g fish_term24bit $fish_term24bit
-            set -U $_tide_left_prompt_display_var (_tide_prompt)" &
+    _tide_last_status=$status _tide_last_pipestatus=$pipestatus if not set -e _tide_self_repainting
+        jobs --query
+        fish --command "_tide_jobs_status=$status CMD_DURATION=$CMD_DURATION COLUMNS=$COLUMNS \
+            fish_bind_mode=$fish_bind_mode set -U $_tide_left_prompt_display_var (_tide_prompt)" &
+        builtin disown
 
         command kill $_tide_last_pid 2>/dev/null
-        set -g _tide_last_pid (jobs --last --pid) # Replace with $last_pid in Fish 3.3.0
-        builtin disown $_tide_last_pid 2>/dev/null
+        set -g _tide_last_pid $last_pid
     end
 
     string unescape $$_tide_left_prompt_display_var
 end
 
-# Double underscores to avoid erasing this function on uninstall
-function __tide_on_fish_exit --on-event fish_exit
+function _tide_on_fish_exit --on-event fish_exit
     set -e $_tide_left_prompt_display_var $_tide_right_prompt_display_var
 end
