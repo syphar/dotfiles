71c6242c71028638fe4885421e296b8ea6432af3
Denis Cornehl <denis@cornehl.org>
2021-09-09 07:47:59 +0200

    fisher update

--- .config/fish/functions/_tide_print_item.fish
+++ .config/fish/functions/_tide_print_item.fish
@@ -2,11 +2,9 @@ function _tide_print_item -a item
     item_bg_color_name=tide_"$item"_bg_color set item_bg_color $$item_bg_color_name
 
     if test "$_tide_which_side_working_on" = left
-        if test "$_tide_last_item" = newline
-            if test "$item" != character
-                set_color $item_bg_color -b normal
-                printf '%s' $tide_left_prompt_prefix
-            end
+        if set -e _tide_last_item_was_newline
+            set_color $item_bg_color -b normal
+            printf '%s' $tide_left_prompt_prefix
         else if test "$item_bg_color" = "$_tide_previous_bg_color"
             set_color $tide_prompt_color_separator_same_color
             printf '%s' $tide_left_prompt_separator_same_color
@@ -14,7 +12,7 @@ function _tide_print_item -a item
             set_color $_tide_previous_bg_color -b $item_bg_color
             printf '%s' $tide_left_prompt_separator_diff_color
         end
-    else if test "$_tide_last_item" = newline
+    else if set -e _tide_last_item_was_newline
         set_color $item_bg_color -b normal
         printf '%s' $tide_right_prompt_prefix
     else if test "$item_bg_color" = "$_tide_previous_bg_color"
@@ -27,12 +25,11 @@ function _tide_print_item -a item
 
     item_color_name=tide_"$item"_color set_color $$item_color_name -b $item_bg_color
 
-    if test "$tide_prompt_pad_items" = true -a "$item" != character
+    if test "$tide_prompt_pad_items" = true
         printf '%s' ' ' $argv[2..] ' '
     else
         printf '%s' $argv[2..]
     end
 
     set -g _tide_previous_bg_color $item_bg_color
-    set -g _tide_last_item $item
 end
