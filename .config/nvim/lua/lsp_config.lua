c8776d60feebe8a3a48a48f1d51353f9cf9d8af8
Denis Cornehl <denis@cornehl.org>
2021-11-06 08:32:36 +0100

    [vim] configure lua language server

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -62,6 +62,39 @@ function cfg.lsp_setup()
 		},
 	})
 
+	local sumneko_root_path = vim.fn.expand("$HOME/src/lua-language-server")
+	local sumneko_binary = sumneko_root_path .. "/bin/MacOS/lua-language-server"
+
+	local runtime_path = vim.split(package.path, ";")
+	table.insert(runtime_path, "lua/?.lua")
+	table.insert(runtime_path, "lua/?/init.lua")
+
+	lsp.sumneko_lua.setup({
+		cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+		settings = {
+			Lua = {
+				runtime = {
+					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+					version = "LuaJIT",
+					-- Setup your lua path
+					path = runtime_path,
+				},
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	})
+
 	lsp.pylsp.setup({
 		capabilities = cfg.updated_capabilities(),
 		on_attach = cfg.lsp_on_attach_without_formatting,
