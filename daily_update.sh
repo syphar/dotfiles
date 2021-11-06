6712bc3e3b8dfed2d2cc596f06d985da0944714f
Denis Cornehl <denis@cornehl.org>
2021-11-06 11:50:42 +0100

    pipx , fix flake8

--- daily_update.sh
+++ daily_update.sh
@@ -26,7 +26,6 @@ xargs -n 1 pipx install < pipx_list.txt 1>/dev/null || echo "fail but OK"
 pipx reinstall-all
 pipx inject python-lsp-server pylsp-mypy
 pipx inject httpie httpie-ntlm
-pipx inject flake8 flake8-docstrings
 
 ## rust environment
 rustup update
