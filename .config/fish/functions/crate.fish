47a57f049012cb5f9cbf8225f0f1c854f3b1bed3
Denis Cornehl <denis@cornehl.org>
2021-11-10 08:15:25 +0100

    [vim] crate search function

--- .config/fish/functions/crate.fish
+++ .config/fish/functions/crate.fish
@@ -1,5 +1,5 @@
 function crate
-    fd --type f . ~/.cargo/registry/index/github.com-1ecc6299db9ec823/ | 
+    fd --type f --exclude ".last_updated" --exclude "config.json" . ~/.cargo/registry/index/github.com-1ecc6299db9ec823/ | 
         sed 's|^.*/||' | 
         fzf
 end
