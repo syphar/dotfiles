04c2230f9473abcf3376a24569fc0b4afb675e79
Denis Cornehl <denis@cornehl.org>
2021-12-01 10:50:54 +0100

    reformat of some files

--- .config/fish/functions/vv.fish
+++ .config/fish/functions/vv.fish
@@ -1,12 +1,12 @@
 # open a file from this projects with vim
 function vv
-  set filename (
-    fd --color=always --type f --type l --hidden --follow --exclude .git 2>/dev/null \
+    set filename (
+    fd --strip-cwd-prefix --color=always --type f --type l --hidden --follow --exclude .git 2>/dev/null \
     | fzf
   )
-  if test $status -eq 0
-      eval $EDITOR $filename
-  end
+    if test $status -eq 0
+        eval $EDITOR $filename
+    end
 
-  commandline -f repaint
+    commandline -f repaint
 end
