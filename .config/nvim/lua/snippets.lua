89ac7d1eae23ab8810bc513e6298e0b58001c7f1
Denis Cornehl <denis@cornehl.org>
2021-12-06 08:03:20 +0100

    [vim] rust snippets

--- .config/nvim/lua/snippets.lua
+++ .config/nvim/lua/snippets.lua
@@ -1,2 +1,21 @@
+local ls = require("luasnip")
+local snippet = ls.snippet
+local text = ls.text_node
+local insert = ls.insert_node
+
+-- examples as documentation:
+-- https://github.com/L3MON4D3/LuaSnip/blob/master/Examples/snippets.lua
+
+ls.snippets = {
+	rust = {
+		snippet("testmod", {
+			text({ "#[cfg(test)]", "mod tests {", "    use super::*;", "    ", "    " }),
+			insert(0),
+			text({ "", "}" }),
+		}),
+	},
+}
+
 -- this loads the snippets from friendly-snippets
+-- https://github.com/rafamadriz/friendly-snippets
 require("luasnip/loaders/from_vscode").lazy_load()
