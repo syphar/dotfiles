f4182d9943920d014f16e935b61fc52882ef045f
Denis Cornehl <denis@cornehl.org>
2021-12-06 19:23:44 +0100

    fisher update

--- .config/fish/conf.d/forgit.plugin.fish
+++ .config/fish/conf.d/forgit.plugin.fish
@@ -95,16 +95,16 @@ function forgit::add -d "git add selector"
     set extract_file "
         sed 's/^[[:space:]]*//' |           # remove leading whitespace
         cut -d ' ' -f 2- |                  # cut the line after the M or ??, this leaves just the filename
-        sed 's/.* -> //' |                  # for rename case
-        sed -e 's/^\\\"//' -e 's/\\\"\$//'  # removes surrounding quotes
+        sed 's/.* -> //'                    # for rename case
     "
+
     set preview "
         set file (echo {} | $extract_file)
         # exit
-        if test (git status -s -- \\\$file | grep '^??') # diff with /dev/null for untracked files
-            git diff --color=always --no-index -- /dev/null \\\$file | $forgit_diff_pager | sed '2 s/added:/untracked:/'
+        if test (git status -s -- \$file | grep '^??') # diff with /dev/null for untracked files
+            git diff --color=always --no-index -- /dev/null \$file | $forgit_diff_pager | sed '2 s/added:/untracked:/'
         else
-            git diff --color=always -- \\\$file | $forgit_diff_pager
+            git diff --color=always -- \$file | $forgit_diff_pager
         end
         "
     set opts "
@@ -115,7 +115,7 @@ function forgit::add -d "git add selector"
     set files (git -c color.status=always -c status.relativePaths=true status -su |
         grep -F -e "$changed" -e "$unmerged" -e "$untracked" |
         sed -E 's/^(..[^[:space:]]*)[[:space:]]+(.*)\$/[\1]  \2/' |   # deal with white spaces internal to fname
-        env FZF_DEFAULT_OPTS="$opts" fzf --preview="fish -c \"$preview\"" |
+        env FZF_DEFAULT_OPTS="$opts" fzf --preview="$preview" |
         sh -c "$extract_file") # for rename case
 
     if test -n "$files"
