f37d75c189b90768970434d686716992b8886469
Denis Cornehl <denis@cornehl.org>
2021-12-10 12:30:10 +0100

    [vim] luasnip cleanup

--- .config/nvim/lua/null_ls_custom/init.lua
+++ .config/nvim/lua/null_ls_custom/init.lua
@@ -3,6 +3,5 @@ local M = {}
 M.curlylint = require("null_ls_custom.curlylint")
 M.gitlint = require("null_ls_custom.gitlint")
 M.pydocstyle = require("null_ls_custom.pydocstyle")
-M.luasnip = require("null_ls_custom.luasnip")
 
 return M
