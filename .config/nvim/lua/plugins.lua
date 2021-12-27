3e4e6cc4320866c3a313e80cb2af296cfb9f0e4e
Denis Cornehl <denis@cornehl.org>
2021-12-27 08:57:39 +0100

    [vim] remove packer bootstrap for potential startup-time impact

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,17 +1,3 @@
-local fn = vim.fn
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local packer_bootstrap = false
-if fn.empty(fn.glob(install_path)) > 0 then
-	packer_bootstrap = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-end
-
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -670,9 +656,5 @@ return require("packer").startup({
 			"jose-elias-alvarez/null-ls.nvim",
 			requires = { "nvim-lua/plenary.nvim" },
 		})
-
-		if packer_bootstrap then
-			require("packer").sync()
-		end
 	end,
 })
