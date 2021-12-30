60adc554b536060084ae4931cfd2b876dd612643
Denis Cornehl <denis@cornehl.org>
2021-12-30 22:31:11 +0100

    [vim] fix wordmotion uppercase motion

--- .config/nvim/after/plugin/wordmotion.lua
+++
@@ -1,4 +0,0 @@
--- uppercase spaces would stop the upper case motion (full words)
-vim.g.wordmotion_uppercase_spaces = { ".", ",", "(", ")", "[", "]", "{", "}", " ", "<", ">", ":" }
--- normal spaces would stop the lower-case (x-case subword) motion
--- let g:wordmotion_spaces = ['\w\@<=-\w\@=', '\.']
---
+++ .config/nvim/after/plugin/wordmotion.vim
@@ -0,0 +1,3 @@
+" uppercase spaces would stop the upper case motion (full words)
+let g:wordmotion_uppercase_spaces = [ '.', ',', '(', ')', '[', ']', '{', '}', ' ', '<', '>', ':' ]
+call wordmotion#reload()
