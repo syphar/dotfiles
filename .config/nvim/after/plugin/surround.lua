19c85b3738398e1d87093aeb2cfeb7b2d81ac6b6
Denis Cornehl <denis@cornehl.org>
2021-12-30 19:06:57 +0100

    [vim] add mapping docs for surround

--- .config/nvim/after/plugin/surround.lua
+++ .config/nvim/after/plugin/surround.lua
@@ -1,1 +1,30 @@
 require("surround").setup({ mappings_style = "sandwich" })
+
+-- There are two keymap modes for normal mode mappings.
+
+--     sandwich and surround and they can be set using the options mentioned below.
+
+-- Normal Mode - Sandwich Mode
+
+--     Provides key mapping to add surrounding characters.( visually select then press s<char> or press sa{motion}{char})
+--     Provides key mapping to replace surrounding characters.( sr<from><to> )
+--     Provides key mapping to delete surrounding characters.( sd<char> )
+--     ss repeats last surround command. (Doesn't work with add)
+
+-- Normal Mode - Surround Mode
+
+--     Provides key mapping to add surrounding characters.( visually select then press s<char> or press ys{motion}{char})
+--     Provides key mapping to replace surrounding characters.( cs<from><to> )
+--     Provides key mapping to delete surrounding characters.( ds<char> )
+
+-- Insert Mode
+
+--     <c-s><char> will insert both pairs in insert mode.
+--     <c-s><char><space> will insert both pairs in insert mode with surrounding whitespace.
+--     <c-s><char><c-s> will insert both pairs on newlines insert mode.
+
+-- IDK I was bored
+
+--     Cycle surrounding quotes type. (stq)
+--     Cycle surrounding brackets type. (stb)
+--     Use <char> == f for adding, replacing, deleting functions.
