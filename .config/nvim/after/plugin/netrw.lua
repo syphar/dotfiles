a133f2fb9964f2dfb14a1b656948a26d40a6a2f4
Denis Cornehl <denis@cornehl.org>
2021-12-30 22:40:50 +0100

    [vim] vim config for old non-lua plugins

--- .config/nvim/after/plugin/netrw.lua
+++
@@ -1,5 +0,0 @@
--- CTRL-6 should go back to the last file, not netrw/vinegar
-vim.g.netrw_altfile = 1
-vim.g.netrw_banner = 0 -- disable banner
-vim.g.netrw_liststyle = 3 -- tree view
-vim.g.netrw_altv = 1 -- open split on the right
