2d299b397f1e064fa24e4331613743289880c0d5
Denis Cornehl <denis@cornehl.org>
2022-03-16 08:20:11 +0100

    [fish] show class in tag-search open

--- .config/fish/functions/ftags.fish
+++ .config/fish/functions/ftags.fish
@@ -6,8 +6,8 @@ function ftags
     end
 
     set -l line (
-        awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3}' tags |
-        fzf --with-nth=2,3
+        awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2"\t"$3"\t"$5}' tags |
+        fzf --delimiter="\t" --with-nth=2,3,5
     ); \
     and set -l filename (string split -f3 \t $line); \
     and set -l tag (string split -f2 \t $line); \
