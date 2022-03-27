868bcb3d79074939014f36f1e9b996455d440640
Denis Cornehl <denis@cornehl.org>
2022-03-27 10:51:29 +0200

    [vim] new taplo lsp

--- .config/nvim/lua/lsp/taplo.lua
+++ .config/nvim/lua/lsp/taplo.lua
@@ -1,8 +1,8 @@
 local cfg = require("lsp")
 
 -- TOML language server with schemas
--- schemas broken for now in LSP
 require("lspconfig").taplo.setup({
+	cmd = { "taplo", "lsp", "stdio" },
 	flags = cfg.global_flags(),
 	capabilities = cfg.capabilities(),
 	on_attach = cfg.lsp_on_attach_without_formatting,
