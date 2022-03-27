784d050dc702e1aefad4a9715f912bd8322bf38b
Denis Cornehl <denis@cornehl.org>
2022-03-27 19:52:59 +0200

    [fish] some config only for interactive

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,19 +3,22 @@ source $XDG_CONFIG_HOME/fish/key_bindings.fish
 source $XDG_CONFIG_HOME/fish/environment.fish
 source $XDG_CONFIG_HOME/fish/colors.fish
 
-fzf_configure_bindings --directory=\cf
 
 # configure franciscolourenco/done
 set -U __done_allow_nongraphical 1
 set -U __done_exclude 'nvim|vim'
 
-/usr/local/bin/direnv hook fish | source
+if status --is-interactive
+  fzf_configure_bindings --directory=\cf
 
-/usr/local/bin/starship init fish | source
+  /usr/local/bin/direnv hook fish | source
 
-/usr/local/bin/zoxide init fish --cmd cd | source
+  /usr/local/bin/starship init fish | source
 
-# Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
-complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+  /usr/local/bin/zoxide init fish --cmd cd | source
+
+  # Enable AWS CLI autocompletion: github.com/aws/aws-cli/issues/1079
+  complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+end
 
 # vim: et ts=2 sts=2 sw=2
