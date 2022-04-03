ddedd979f25db169d67a3bc2193f75bad11b5c66
Denis Cornehl <denis@cornehl.org>
2022-04-03 06:45:37 +0200

    [starship] small improvement for heroku module

--- bin/starship_heroku.lua
+++ bin/starship_heroku.lua
@@ -1,36 +1,31 @@
-stringy = require("stringy")
-
-local app = os.getenv("HEROKU_APP")
-if app then
-	app = app:gsub("automagically%-", " ")
-	app = app:gsub("medicaljobs%-", "MJ ")
-	app = app:gsub("thermondo%-", "⭘ ")
-else 
-	app = ""
+function get_app()
+	return os.getenv("HEROKU_APP") or ""
 end
 
-local netrc_file = assert(io.open(os.getenv("HOME") .. "/.netrc", "r"))
-local last_machine_was_heroku = false
-local account = ""
-for line in netrc_file:lines() do
-	local line = stringy.strip(line)
-	local key, value = table.unpack(stringy.split(line, " "))
+function get_account() 
+	local netrc_file = assert(io.open(os.getenv("HOME") .. "/.netrc", "r"))
+	local netrc = netrc_file:read("*all")
+	netrc_file:close()
 
-	if key == "machine" then
-		if value == "api.heroku.com" then
-			last_machine_was_heroku = true
-		else
-			last_machine_was_heroku = false
+	local last_token = nil
+	local last_machine_was_heroku = false
+
+	for token in netrc:gmatch("[^%s]+") do
+		if last_token == "machine" then
+			last_machine_was_heroku = (token == "api.heroku.com")
+		elseif last_token == "login" and last_machine_was_heroku then
+			return token
 		end
-	elseif key == "login" then
-		if last_machine_was_heroku then
-			account = value
-			break
-		end
+		last_token = token
 	end
 end
-netrc_file:close()
 
+local app = get_app()
+app = app:gsub("automagically%-", " ")
+app = app:gsub("medicaljobs%-", "MJ ")
+app = app:gsub("thermondo%-", "⭘ ")
+
+local account = get_account()
 if account == "denis@cornehl.org" then
 	account = ""
 elseif account == "denis.cornehl@thermondo.de" then
