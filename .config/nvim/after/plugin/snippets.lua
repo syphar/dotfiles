dccbc583712dffeda79ce3748a168a6cf954a702
Denis Cornehl <denis@cornehl.org>
2022-04-07 07:04:38 +0200

    whitespace

--- .config/nvim/after/plugin/snippets.lua
+++ .config/nvim/after/plugin/snippets.lua
@@ -46,7 +46,7 @@ ls.add_snippets("python", {
 require("luasnip/loaders/from_vscode").lazy_load()
 
 vim.cmd([[
-	imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>' 
+	imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
 	inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
 
 	snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
