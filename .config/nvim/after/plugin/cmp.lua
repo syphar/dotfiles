267147c69149a17bc6beca01bdcc710be6ff1e8c
Denis Cornehl <denis@cornehl.org>
2022-04-14 09:05:00 +0200

    [vim] cmp, update for new version

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -14,7 +14,7 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body)
 		end,
 	},
-	mapping = {
+	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
@@ -47,7 +47,7 @@ cmp.setup({
 		-- 		fallback()
 		-- 	end
 		-- end, { "i", "s" }),
-	},
+	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lua" },
 		{ name = "nvim_lsp" },
