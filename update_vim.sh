a4381d22e9a9e4aa54bfc7bbe0c4e8a4f0dcd07e
Denis Cornehl <denis@cornehl.org>
2022-04-16 08:49:27 +0200

    [daily] just install all treesitter parsers

--- update_vim.sh
+++ update_vim.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c "TSInstallSync maintained" -c "TSUpdateSync" -c "quitall"
+nvim --headless -c "TSInstallSync all" -c "TSUpdateSync" -c "quitall"
