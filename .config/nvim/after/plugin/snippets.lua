13a555bf1f9cb4a655c1ec803071c9146ea6b3d2
Denis Cornehl <denis@cornehl.org>
2022-04-17 20:41:10 +0200

    [vim] update snippets

--- .config/nvim/after/plugin/snippets.lua
+++ .config/nvim/after/plugin/snippets.lua
@@ -1,6 +1,8 @@
 local ls = require("luasnip")
 local snippet = ls.snippet
+local function_ = ls.function_node
 local text = ls.text_node
+local fmt = require("luasnip.extras.fmt").fmt
 local insert = ls.insert_node
 
 ls.config.set_config({
@@ -33,16 +35,33 @@ ls.add_snippets("python", {
 	snippet("loggermod", {
 		text({ "logger = logging.getLogger(__name__)" }),
 	}),
+	snippet("pdb", { text("__import__('pdb').set_trace()") }),
+	snippet("ipdb", { text("__import__('ipdb').set_trace()") }),
 }, {
 	key = "my_python_snippets",
 })
 
+ls.add_snippets("lua", {
+	snippet(
+		"req",
+		fmt([[local {} = require("{}")]], {
+			function_(function(args)
+				local splits = vim.split(args[1][1], ".", true)
+				return splits[#splits] or ""
+			end, { 1 }),
+			insert(1),
+		})
+	),
+}, {
+	key = "my_lua_snippets",
+})
+
 vim.cmd([[
 	imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>'
-	inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
+	inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<CR>
 
-	snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-	snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
+	snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<CR>
+	snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<CR>
 
 	imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
 	smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
