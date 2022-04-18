34537bd98c044d006c2f1d95cf0cc681ba9737f8
Denis Cornehl <denis@cornehl.org>
2022-04-18 11:20:00 +0200

    [fish] heroku complete, use more builtins

--- .config/fish/completions/heroku.fish
+++ .config/fish/completions/heroku.fish
@@ -6,24 +6,6 @@
 # I don't know yet how this is generated.
 set -l heroku_ac_commands_path $HOME/Library/Caches/heroku/autocomplete/commands
 
-function __fish_heroku_using_command
-    set -l cmd (commandline -opc)
-    if test (count $cmd) -gt 1
-        if test $argv[1] = $cmd[2]
-            return 0
-        end
-    end
-    return 1
-end
-
-function __fish_heroku_needs_command
-    set -l cmd (commandline -opc)
-    if test (count $cmd) -eq 1
-        return 0
-    end
-    return 1
-end
-
 function __list_heroku_complete_options
     set -l cmd (commandline -opc)
     # for now cache all autocomplete options 
@@ -45,7 +27,7 @@ function __fish_list_installed_addons
         -- heroku addons --json | __get_only_name_from_json
 end
 
-set -l heroku_looking -c heroku -n __fish_heroku_needs_command
+set -l heroku_looking -c heroku -n __fish_use_subcommand
 
 for line in (/bin/cat $heroku_ac_commands_path)
     set -l tokens (string split --no-empty ' ' $line)
@@ -59,9 +41,9 @@ for line in (/bin/cat $heroku_ac_commands_path)
 
             if test $long_arg = 'app' 
                 # special case to add short `-a` arg for `--app` for now
-                complete -c heroku -n "__fish_heroku_using_command $cmd" -s a -l app -xa '(__list_heroku_complete_options)'
+                complete -c heroku -n "__fish_seen_subcommand_from $cmd" -s a -l app -xa '(__list_heroku_complete_options)'
             else
-                complete -c heroku -n "__fish_heroku_using_command $cmd" -l $long_arg -xa '(__list_heroku_complete_options)'
+                complete -c heroku -n "__fish_seen_subcommand_from $cmd" -l $long_arg -xa '(__list_heroku_complete_options)'
             end
         end
     end
@@ -69,5 +51,5 @@ end
 
 # additional completions that don't seem to work with autocomplete:options
 for cmd in 'addons:destroy' 'addons:info'
-    complete -c heroku -n "__fish_heroku_using_command $cmd" -fa '(__fish_list_installed_addons)'
+    complete -c heroku -n "__fish_seen_subcommand_from $cmd" -fa '(__fish_list_installed_addons)'
 end
