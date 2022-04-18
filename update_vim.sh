f633a606692f454f1e8b22e171b9227a2c25fd00
Denis Cornehl <denis@cornehl.org>
2022-04-18 19:44:13 +0200

    [daily] styling

--- update_vim.sh
+++ update_vim.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c "TSUpdateSync" -c "quitall"
+nvim --headless \
+    -c 'autocmd User PackerComplete quitall' \
+    -c 'PackerSync'
+
+nvim --headless \
+    -c "TSUpdateSync" \
+    -c "quitall"
