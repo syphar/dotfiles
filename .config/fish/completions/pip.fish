8f1eb9c4764a7ef3603d015f3266ca9cec2868bf
Denis Cornehl <denis@cornehl.org>
2022-04-19 14:45:19 +0200

    [fish] disable default pip autocomplete, pre-filter package names before giving to fish

--- .config/fish/completions/pip.fish
+++ .config/fish/completions/pip.fish
@@ -1,10 +1,12 @@
-# default from fish-shell source
-pip completion --fish 2>/dev/null | source
+# custom pip completion because default is too slow
 
-# custom complete package names
+for cmd in 'install' 'uninstall' 
+    complete -c pip -n __fish_use_subcommand -xa $cmd
+end
+
 function __list_pypi_packages
     set -l partial_name (commandline -t)
-    rg "^$parital_name" --smart-case ~/.cache/pypi_packages.txt
+    rg "^$partial_name" --smart-case ~/.cache/pypi_packages.txt
 end
 
 complete -c pip -n "__fish_seen_subcommand_from install" -fa '(__list_pypi_packages)'
