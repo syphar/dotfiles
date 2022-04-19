a1bda410597e1fed25cd43c00e8e7a01f17551c3
Denis Cornehl <denis@cornehl.org>
2022-04-19 09:32:22 +0200

    [fish] prefilter package-list for pip install package name complete

--- .config/fish/completions/pip.fish
+++ .config/fish/completions/pip.fish
@@ -3,7 +3,8 @@ pip completion --fish 2>/dev/null | source
 
 # custom complete package names
 function __list_pypi_packages
-    /bin/cat ~/.cache/pypi_packages.txt
+    set -l partial_name (commandline -t)
+    rg "^$parital_name" --smart-case ~/.cache/pypi_packages.txt
 end
 
 complete -c pip -n "__fish_seen_subcommand_from install" -fa '(__list_pypi_packages)'
