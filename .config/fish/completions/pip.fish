719950bbab5a6b1c928d4e24d140df0df75d31cc
Denis Cornehl <denis@cornehl.org>
2022-04-20 22:25:08 +0200

    [fish] fun experiment: complete versions in pip install

--- .config/fish/completions/pip.fish
+++ .config/fish/completions/pip.fish
@@ -1,12 +1,23 @@
 # custom pip completion because default is too slow
 
-for cmd in 'install' 'uninstall' 
+for cmd in install uninstall
     complete -c pip -n __fish_use_subcommand -xa $cmd
 end
 
 function __list_pypi_packages
     set -l partial_name (commandline -t)
-    rg "^$partial_name" --smart-case ~/.cache/pypi_packages.txt
+
+    if string match -q "*==*" $partial_name
+        set -l package (string match -g --regex "([^=]+)==.*" $partial_name)
+        curl -Lfs https://pypi.org/pypi/$package/json | \
+            jq -r '.releases|keys[]' | \
+            # prefix with package name again so complete matches
+            sed -e "s/^/$package==/" | \
+            # semver sort: https://stackoverflow.com/a/63027058/1194456
+            sort -r -t "." -k1,1n -k2,2n -k3,3n 
+    else
+        rg "^$partial_name" --smart-case ~/.cache/pypi_packages.txt
+    end
 end
 
-complete -c pip -n "__fish_seen_subcommand_from install" -fa '(__list_pypi_packages)'
+complete -c pip -n "__fish_seen_subcommand_from install" -fka '(__list_pypi_packages)'
