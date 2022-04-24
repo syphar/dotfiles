3947c068c3a096630947e3ff1269bdb3c0c66b41
Denis Cornehl <denis@cornehl.org>
2022-04-24 10:45:14 +0200

    [vim] fix incline window location after focus.nvim changes

--- .config/nvim/after/plugin/ui.lua
+++ .config/nvim/after/plugin/ui.lua
@@ -1,4 +1,18 @@
-require("focus").setup({ cursorline = false, signcolumn = false })
+require("focus").setup({ cursorline = false, signcolumn = false, autoresize = false })
+
+-- default focus.nvim autocmd, 
+-- calling WinScrolled afterwards so _incline_ floats
+-- are updated correctly
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	pattern = "*",
+	group = vim.api.nvim_create_augroup("focus_nvim", {}),
+	callback = function()
+		vim.schedule(function()
+			require("focus").resize()
+			vim.cmd([[doautocmd WinScrolled]])
+		end)
+	end,
+})
 
 -- auto-adjust splits when window is resized
 -- https://vi.stackexchange.com/questions/201/make-panes-resize-when-host-window-is-resized
