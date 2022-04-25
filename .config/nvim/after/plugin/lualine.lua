5e7656084c209e80f88b917df65df0f73f82c24c
Denis Cornehl <denis@cornehl.org>
2022-04-25 13:03:39 +0200

    [vim] cleanup

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,5 +1,3 @@
--- https://github.com/sindresorhus/cli-spinners
-local spinner_symbols = { "⠋ ", "⠙ ", "⠹ ", "⠸ ", "⠼ ", "⠴ ", "⠦ ", "⠧ ", "⠇ ", "⠏ " }
 require("lualine").setup({
 	options = {
 		theme = "kanagawa",
@@ -37,56 +35,12 @@ require("lualine").setup({
 				shorting_target = 20,
 			},
 		},
-		lualine_x = {
-			-- {
-			-- 	"lsp_progress",
-			-- 	display_components = {
-			-- 		-- "lsp_client_name",
-			-- 		{
-			-- 			"title",
-			-- 			"message",
-			-- 			-- for later: https://changaco.oy.lc/unicode-progress-bars/
-			-- 			"percentage",
-			-- 		},
-			-- 		"spinner",
-			-- 	},
-			-- 	spinner_symbols = spinner_symbols,
-			-- 	timer = { progress_enddelay = 0, spinner = 80, lsp_client_name_enddelay = 0 },
-			-- 	message = { commenced = "", completed = "" },
-			-- 	separators = {
-			-- 		component = " ",
-			-- 		progress = " ",
-			-- 		lsp_client_name = { pre = "[", post = "] " },
-			-- 		title = { pre = "", post = " " },
-			-- 		percentage = { pre = "", post = "%% " },
-			-- 		message = { pre = "(", post = ") " },
-			-- 	},
-			-- 	fmt = function(str)
-			-- 		-- FIXME ugly hack.
-			-- 		-- Something in either the sumneko_lua & rust_analyzer LSPs or
-			-- 		-- lualine_lsp_progress leads to some statuses being duplicated.
-			-- 		-- This happens only on workspace load / startup.
-			-- 		--
-			-- 		-- This codes just de-duplicates words, which partially fixes
-			-- 		-- this issue.
-			-- 		local output = {}
-			-- 		local seen = {}
-
-			-- 		for _, word in ipairs(vim.split(str, " ")) do
-			-- 			if seen[word] == nil then
-			-- 				table.insert(output, word)
-			-- 				seen[word] = true
-			-- 			end
-			-- 		end
-			-- 		return table.concat(output, " ")
-			-- 	end,
-			-- },
-		},
+		lualine_x = {},
 		lualine_y = {
 			{ "diagnostics", sources = { "nvim_diagnostic" } },
 			{ "filetype", icon_only = false },
 		},
-		lualine_z = { "LineNoIndicator", "location", },
+		lualine_z = { "LineNoIndicator", "location" },
 	},
 	inactive_sections = {
 		lualine_a = {},
@@ -96,7 +50,7 @@ require("lualine").setup({
 		lualine_y = {
 			{ "filetype", icon_only = false },
 		},
-		lualine_z = { "LineNoIndicator", "location", },
+		lualine_z = { "LineNoIndicator", "location" },
 	},
 	tabline = {},
 	extensions = {
