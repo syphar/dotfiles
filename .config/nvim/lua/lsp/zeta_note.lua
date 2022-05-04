fd12827f23072d112fb65a8b8e40eba33334093a
Denis Cornehl <denis@cornehl.org>
2022-05-04 13:31:56 +0200

    [vim] fix zeta-note server

--- .config/nvim/lua/lsp/zeta_note.lua
+++ .config/nvim/lua/lsp/zeta_note.lua
@@ -4,7 +4,4 @@ require("lspconfig").zeta_note.setup({
 	flags = cfg.global_flags(),
 	capabilities = cfg.capabilities(),
 	on_attach = cfg.lsp_on_attach_without_formatting,
-	cmd = {
-		vim.fn.expand("$HOME/.local/bin/zeta-note"),
-	},
 })
