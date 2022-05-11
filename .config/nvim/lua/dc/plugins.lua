781be203c5b121e90145ef0a071857d3314f5e8d
Denis Cornehl <denis@cornehl.org>
2022-05-11 07:09:01 +0200

    [vim] back to original treesitter-context

--- .config/nvim/lua/dc/plugins.lua
+++ .config/nvim/lua/dc/plugins.lua
@@ -41,7 +41,7 @@ return require("packer").startup({
 		use("nvim-treesitter/nvim-treesitter")
 		use("nvim-treesitter/playground")
 		use("nvim-treesitter/nvim-treesitter-textobjects")
-		use("lewis6991/nvim-treesitter-context")
+		use("romgrk/nvim-treesitter-context")
 		use("mfussenegger/nvim-treehopper")
 		use("RRethy/nvim-treesitter-textsubjects")
 		use("monaqa/dial.nvim")
