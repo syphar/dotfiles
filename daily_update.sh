18092ddc7067a21fb720c236234b33f752e14338
Denis Cornehl <denis@cornehl.org>
2022-05-15 09:54:58 +0200

    add requests to gobal ipython

--- daily_update.sh
+++ daily_update.sh
@@ -36,7 +36,7 @@ pipx reinstall-all
 pipx inject python-lsp-server pylsp-mypy
 pipx inject httpie httpie-ntlm
 pipx inject poetry poetry-dynamic-versioning
-pipx inject ipython rich
+pipx inject ipython rich requests
 
 ./update_generated_autocompletes.sh
 
