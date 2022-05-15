125256b882d0f7ac78b2b7bb230ffe029f17a618
Denis Cornehl <denis@cornehl.org>
2022-05-15 08:37:50 +0200

    [vim] python docs, cleanup

--- .config/nvim/plugin/python_docs.lua
+++ .config/nvim/plugin/python_docs.lua
@@ -1,18 +1,13 @@
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local finders = require("telescope.finders")
-local make_entry = require("telescope.make_entry")
 local pickers = require("telescope.pickers")
-local previewers = require("telescope.previewers")
-local utils = require("telescope.utils")
 local entry_display = require("telescope.pickers.entry_display")
-local strings = require("plenary.strings")
-local Path = require("plenary.path")
 local conf = require("telescope.config").values
 
 local python_package_documentation = function()
 	local displayer = entry_display.create({
-		separator = " | ",
+		separator = "▏",
 		items = {
 			{ width = 15 },
 			{ width = 20 },
@@ -21,7 +16,7 @@ local python_package_documentation = function()
 	})
 
 	pickers.new({}, {
-		prompt_title = "ls ",
+		prompt_title = "python package documentation",
 		finder = finders.new_oneshot_job({
 			"python3",
 			vim.fn.expand("$HOME/src/dotfiles/print_documentation_urls.py"),
@@ -41,7 +36,6 @@ local python_package_documentation = function()
 		sorter = conf.generic_sorter({}),
 		attach_mappings = function(_, _)
 			actions.select_default:replace(function(prompt_bufnr)
-				-- local cwd = action_state.get_current_picker(prompt_bufnr).cwd
 				local selection = action_state.get_selected_entry()
 				if selection == nil then
 					return
