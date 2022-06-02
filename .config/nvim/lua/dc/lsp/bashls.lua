3089cea2b6dc19597519aae2c2a933d6c11447c6
Denis Cornehl <denis@cornehl.org>
2022-06-02 08:41:19 +0200

    [vim] disable formatting for bash-ls

--- .config/nvim/lua/dc/lsp/bashls.lua
+++ .config/nvim/lua/dc/lsp/bashls.lua
@@ -3,5 +3,5 @@ local cfg = require("dc.lsp")
 require("lspconfig").bashls.setup({
 	flags = cfg.global_flags(),
 	capabilities = cfg.capabilities(),
-	on_attach = cfg.lsp_on_attach,
+	on_attach = cfg.lsp_on_attach_without_formatting,
 })
