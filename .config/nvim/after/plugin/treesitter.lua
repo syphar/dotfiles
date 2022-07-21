2f1e151e067aaed2515327335e9127236fdf6628
Denis Cornehl <denis@cornehl.org>
2022-07-21 17:12:44 +0200

    [vim] markdown treesitter is still broken

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -41,7 +41,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		local parsers = require("nvim-treesitter.parsers")
 
 		local lang = parsers.get_buf_lang()
-		if parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) then
+		if lang ~= "markdown" and parsers.get_parser_configs()[lang] and not parsers.has_parser(lang) then
 			vim.schedule_wrap(function()
 				vim.cmd("TSInstallSync " .. lang)
 			end)()
