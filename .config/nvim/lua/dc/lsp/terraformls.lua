92aa162dc2fba21cbaddcfb397936fbcb5e2af5f
Denis Cornehl <denis@cornehl.org>
2022-07-25 06:54:16 +0200

    [vim] terraform-ls settings

--- .config/nvim/lua/dc/lsp/terraformls.lua
+++ .config/nvim/lua/dc/lsp/terraformls.lua
@@ -4,4 +4,10 @@ require("lspconfig").terraformls.setup({
 	flags = cfg.global_flags(),
 	capabilities = cfg.capabilities(),
 	on_attach = cfg.lsp_on_attach_without_formatting,
+	init_options = {
+		experimentalFeatures = {
+			validateOnSave = true,
+			prefillRequiredFields = true,
+		},
+	},
 })
