93a9e34aa6816f9b0b99866681310398df32ca3e
Denis Cornehl <denis@cornehl.org>
2022-08-05 14:36:14 +0200

    [vim] disable some default plugins

--- .config/nvim/lua/dc/vim_options.lua
+++ .config/nvim/lua/dc/vim_options.lua
@@ -3,6 +3,27 @@ vim.g.do_filetype_lua = 1
 -- disable old filetype.vim
 vim.g.did_load_filetypes = 0
 
+-- disable some default plugins
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+
 vim.cmd("filetype plugin indent on")
 vim.cmd("syntax enable")
 vim.o.termguicolors = true
