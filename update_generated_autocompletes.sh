e8fa0324709af4080d5e7256717ac30dc2add6b0
Denis Cornehl <denis@cornehl.org>
2022-09-04 08:46:51 +0200

    [fish] autocompletes for dslr

--- update_generated_autocompletes.sh
+++ update_generated_autocompletes.sh
@@ -3,3 +3,4 @@ set -exuo pipefail
 
 poetry completions fish > ~/.config/fish/completions/poetry.fish
 poe _fish_completion > ~/.config/fish/completions/poe.fish
+_DSLR_COMPLETE=fish_source dslr > ~/.config/fish/completions/dslr.fish
