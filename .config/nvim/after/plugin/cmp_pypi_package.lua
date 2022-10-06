2072276884a1f52ac3d8f7ba9e17c6ed79ed39cf
Denis Cornehl <denis@cornehl.org>
2022-10-06 07:26:17 +0200

    [vim] fix pypi package autocomplete

--- .config/nvim/after/plugin/cmp_pypi_package.lua
+++ .config/nvim/after/plugin/cmp_pypi_package.lua
@@ -13,31 +13,29 @@ end
 
 function source:complete(request, callback)
 	local q = string.sub(request.context.cursor_before_line, request.offset)
-	Job
-		:new({
-			"rg",
-			"^" .. q,
-			"--smart-case",
-			"--max-count",
-			10,
-			vim.env.HOME .. "/.cache/pypi_packages.txt",
-			on_stderr = function(_, data, _)
-				print("GOT ERROR FROM ripgrep : " .. data)
-			end,
-			on_exit = function(job, _, _)
-				local items = {}
-				for _, line in ipairs(job:result()) do
-					table.insert(items, {
-						label = line,
-					})
-				end
-				callback({
-					items = items,
-					isIncomplete = false,
+	Job:new({
+		"rg",
+		"^" .. q,
+		"--smart-case",
+		"--max-count",
+		10,
+		vim.env.HOME .. "/.cache/pypi_packages.txt",
+		on_stderr = function(_, data, _)
+			print("GOT ERROR FROM ripgrep : " .. data)
+		end,
+		on_exit = function(job, _, _)
+			local items = {}
+			for _, line in ipairs(job:result()) do
+				table.insert(items, {
+					label = line,
 				})
-			end,
-		})
-		:start()
+			end
+			callback({
+				items = items,
+				isIncomplete = true,
+			})
+		end,
+	}):start()
 end
 
 require("cmp").register_source("pypi_package", source.new())
