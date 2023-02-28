441456ea15164d98bef5e8e08f342f024c41e160
Denis Cornehl <denis@cornehl.org>
2023-02-28 07:51:38 +0100

    [vim] python, inject SQL hilight

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -5,3 +5,9 @@
   arguments: (argument_list (string) @soql ))
   (#match? @_sf_query "^sf\.?.*\.query.*$")
 )
+
+(
+    (string) @sql
+    (#match? @sql "^.*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|ALTER.*$")
+    (#offset! @sql 0 1 0 -1)
+)
