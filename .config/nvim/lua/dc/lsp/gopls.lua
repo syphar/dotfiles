101dca140de008635747970729e7a267119ecc7a
Denis Cornehl <denis@cornehl.org>
2023-03-01 11:03:41 +0100

    [vim] go / inlay hints

--- .config/nvim/lua/dc/lsp/gopls.lua
+++ .config/nvim/lua/dc/lsp/gopls.lua
@@ -4,4 +4,16 @@ require("lspconfig").gopls.setup({
 	flags = cfg.global_flags(),
 	capabilities = cfg.capabilities(),
 	on_attach = cfg.lsp_on_attach_without_formatting,
+	settings = {
+		gopls = {
+			hints = {
+				assignVariableTypes = true,
+				compositeLiteralFields = true,
+				constantValues = true,
+				functionTypeParameters = true,
+				parameterNames = true,
+				rangeVariableTypes = true,
+			},
+		},
+	},
 })
