e3f3b062453b00165509d08507912b5ef9bc1389
Denis Cornehl <denis@cornehl.org>
2023-03-08 17:10:28 +0100

    [vim] extend go & javascript contrext

--- .config/nvim/queries/javascript/context.scm
+++ .config/nvim/queries/javascript/context.scm
@@ -7,12 +7,16 @@
 ([
   (class_declaration)
   (function_declaration)
+  (lexical_declaration)
   (method_definition)
   (arrow_function)
   (else_clause)
   (while_statement)
+  (expression_statement)
   (jsx_element)
   (jsx_self_closing_element)
   (call_expression)
+  (object)
+  (pair)
 ] @context)
 
