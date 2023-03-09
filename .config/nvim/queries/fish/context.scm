bb14de60c2b9037a776f56645a81c5a64e01ffaf
Denis Cornehl <denis@cornehl.org>
2023-03-09 17:35:54 +0100

    [vim] context, fish querie

---
+++ .config/nvim/queries/fish/context.scm
@@ -0,0 +1,15 @@
+;; extends
+
+([
+  (function_definition)
+  (for_statement)
+  (begin_statement)
+  (while_statement)
+  (switch_statement)
+  (case_clause)
+  (else_if_clause)
+  (else_clause)
+  (if_statement)
+  (command)
+] @context)
+
