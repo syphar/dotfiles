eadf609b00f2f446f19837f2e4bc4c58d7ed07b4
Denis Cornehl <denis@cornehl.org>
2023-03-21 07:46:09 +0100

    [vim] sql query hilight for Insert/update

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -8,6 +8,6 @@
 
 (
     (string) @sql
-    (#match? @sql "^.*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|ALTER.*$")
+    (#match? @sql "^\w*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
     (#offset! @sql 0 1 0 -1)
 )
