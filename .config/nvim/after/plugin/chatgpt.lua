7b277a8a5a6c25e4a4d3ac7458f02b47480a3a31
Denis Cornehl <denis@cornehl.org>
2023-04-14 07:23:35 +0200

    [vim] chatchpt mapping

--- .config/nvim/after/plugin/chatgpt.lua
+++ .config/nvim/after/plugin/chatgpt.lua
@@ -1,23 +1,6 @@
 local chatgpt = require("chatgpt")
 chatgpt.setup({
 	max_line_length = 88,
-	-- yank_register = "+",
-	-- keymaps = {
-	-- 	close = "<esc>",
-	-- 	submit = "<C-Enter>",
-	-- 	-- the rest is default
-	-- 	yank_last = "<C-y>",
-	-- 	yank_last_code = "<C-k>",
-	-- 	scroll_up = "<C-u>",
-	-- 	scroll_down = "<C-d>",
-	-- 	toggle_settings = "<C-o>",
-	-- 	new_session = "<C-n>",
-	-- 	cycle_windows = "<Tab>",
-	-- 	-- in the Sessions pane
-	-- 	select_session = "<Space>",
-	-- 	rename_session = "r",
-	-- 	delete_session = "d",
-	-- },
 })
 
 vim.keymap.set({ "n", "v" }, "<leader>v", chatgpt.edit_with_instructions)
