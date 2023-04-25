d3cd1baf0f97c91070e4d1a77050d971f1e2ea98
Denis Cornehl <denis@cornehl.org>
2023-04-25 08:20:20 +0200

    [vim] make python/SQL injection case sensitive

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -8,6 +8,6 @@
 
 (
     (string) @sql
-    (#match? @sql "^\w*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
+    (#vim-match? @sql "\C^\w*SELECT|FROM|INNER JOIN|WHERE|CREATE|DROP|INSERT|UPDATE|ALTER.*$")
     (#offset! @sql 0 1 0 -1)
 )
