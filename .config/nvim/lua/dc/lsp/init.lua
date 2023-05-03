dd85ba0a111de556a6d6d0c3e9b25506f1adbfa3
Denis Cornehl <denis@cornehl.org>
2023-05-03 08:25:48 +0200

    [vim] fix lsp error icons after nerdfont update

--- .config/nvim/lua/dc/lsp/init.lua
+++ .config/nvim/lua/dc/lsp/init.lua
@@ -112,7 +112,7 @@ end
 function cfg.lsp_setup()
 	-- nice diagnostic icons in sign-column
 	-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#neovim-060-1
-	local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+	local signs = { Error = "", Warn = "", Hint = "", Info = "" }
 	for type, icon in pairs(signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
