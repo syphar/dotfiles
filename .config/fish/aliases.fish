73b4e97b64e1e1106857f6c3f3628ab9a0c52e72
Denis Cornehl <denis@cornehl.org>
2023-06-28 08:11:40 +0200

    [fish] alias for poetry instlal

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -4,6 +4,7 @@ alias pipupgrade="pip freeze | grep = | cut -d = -f 1 | xargs pip install -U"
 alias pipupgrade2="cat requirements.txt | grep = | cut -d = -f 1 | xargs pip install -U"
 alias pipirt="pip install --upgrade pip wheel setuptools && pip install -r requirements_test.txt"
 abbr -ag pl poetry lock --no-update
+abbr -ag pi poetry install
 
 alias tiga='tig --all'
 alias tigs='tig status'
