2defdf3ad4f5ecc8c53c7f65e2d5639398d73d90
Denis Cornehl <denis@cornehl.org>
2023-10-05 20:35:59 +0200

    [vim] disable hack-autocmd to disable cursorline to fix LIR

--- .config/nvim/lua/dc/plugins/focus.lua
+++ .config/nvim/lua/dc/plugins/focus.lua
@@ -17,21 +17,21 @@ return {
 		-- This autocmd below is a copy of it that is calling WinScrolled
 		-- afterwards so _incline_ floats are updated correctly.
 		-- upstream fix: https://github.com/beauwilliams/focus.nvim/pull/83
-		vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-			pattern = "*",
-			group = vim.api.nvim_create_augroup("focus_nvim", {}),
-			callback = function()
-				vim.schedule(function()
-					require("focus").resize()
-					vim.api.nvim_exec_autocmds("WinScrolled", {})
-					-- HACK:
-					-- even when disabled globally, the cursorline
-					-- is still activated by a plugin, probably
-					-- this one.
-					-- So I disable it here again
-					vim.opt.cursorline = false
-				end)
-			end,
-		})
+		-- vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+		-- 	pattern = "*",
+		-- 	group = vim.api.nvim_create_augroup("focus_nvim", {}),
+		-- 	callback = function()
+		-- 		vim.schedule(function()
+		-- 			require("focus").resize()
+		-- 			vim.api.nvim_exec_autocmds("WinScrolled", {})
+		-- 			-- HACK:
+		-- 			-- even when disabled globally, the cursorline
+		-- 			-- is still activated by a plugin, probably
+		-- 			-- this one.
+		-- 			-- So I disable it here again
+		-- 			vim.opt.cursorline = false
+		-- 		end)
+		-- 	end,
+		-- })
 	end,
 }
