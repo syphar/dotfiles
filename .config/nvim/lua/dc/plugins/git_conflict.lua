b0058ae018d5782b410250c17613c7495437e23e
Denis Cornehl <denis@cornehl.org>
2023-10-10 06:38:44 +0200

    [vim] un-lazy git-conflict

--- .config/nvim/lua/dc/plugins/git_conflict.lua
+++ .config/nvim/lua/dc/plugins/git_conflict.lua
@@ -1,6 +1,7 @@
 return {
 	"akinsho/git-conflict.nvim",
-	event = "VeryLazy",
+	-- event = "VeryLazy",
+	version = "*",
 	opts = {
 		default_mappings = true, -- disable buffer local mapping created by this plugin
 		disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
