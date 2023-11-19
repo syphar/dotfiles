5d9538342ae38faacede7325f10129feb002c8a6
Denis Cornehl <denis@cornehl.org>
2023-11-19 07:20:18 +0100

    [vim] migrate back from flash.nvim to a new hop.nvim fork

---
+++ .config/nvim/lua/dc/plugins/hop.lua
@@ -0,0 +1,41 @@
+local function hop_after(method)
+	local HintDirection = require("hop.hint").HintDirection
+	method({ direction = HintDirection.AFTER_CURSOR, current_line_only = false })
+end
+
+local function hop_before(method)
+	local HintDirection = require("hop.hint").HintDirection
+	method({ direction = HintDirection.BEFORE_CURSOR, current_line_only = false })
+end
+
+return {
+	"smoka7/hop.nvim",
+	version = "*",
+	opts = {},
+	keys = {
+		{
+			"<leader>hw",
+			function()
+				hop_after(require("hop").hint_words)
+			end,
+		},
+		{
+			"<leader>hW",
+			function()
+				hop_before(require("hop").hint_words)
+			end,
+		},
+		{
+			"<leader>hl",
+			function()
+				hop_after(require("hop").hint_lines_skip_whitespace)
+			end,
+		},
+		{
+			"<leader>hL",
+			function()
+				hop_before(require("hop").hint_lines_skip_whitespace)
+			end,
+		},
+	},
+}
