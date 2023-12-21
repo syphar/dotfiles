d70e71f18067298288d54ce1ccf91eccd84b2ce0
Denis Cornehl <denis@cornehl.org>
2023-12-21 06:49:07 +0100

    vim: trim inner scope in context

--- .config/nvim/lua/dc/plugins/treesitter_context.lua
+++ .config/nvim/lua/dc/plugins/treesitter_context.lua
@@ -7,6 +7,7 @@ return {
 		throttle = true,
 		line_numbers = false,
 		max_lines = 10,
+		trim_scope = "inner", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
 		multiline_threshold = 10,
 		mode = "topline", -- choices: 'cursor', 'topline'
 		patterns = {
