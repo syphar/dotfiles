e818d557cd18ee8ede349d8950400ed82f39ee58
Denis Cornehl <denis@cornehl.org>
2024-03-14 08:45:46 +0100

    vim: optimize trouble.nvim settings

--- .config/nvim/lua/dc/plugins/trouble.lua
+++ .config/nvim/lua/dc/plugins/trouble.lua
@@ -4,7 +4,7 @@ return {
 	opts = {
 		position = "right", -- position of the list can be: bottom, top, left, right
 		height = 10, -- height of the trouble list when position is top or bottom
-		width = 50, -- width of the list when position is left or right
+		width = 60, -- width of the list when position is left or right
 		icons = true, -- use devicons for filenames
 		mode = "workspace_diagnostics", -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
 		severity = nil, -- nil (ALL) or vim.diagnostic.severity.ERROR | WARN | INFO | HINT
@@ -41,7 +41,7 @@ return {
 		indent_lines = true, -- add an indent guide below the fold icons
 		win_config = { border = "single" }, -- window configuration for floating windows. See |nvim_open_win()|.
 		auto_open = false, -- automatically open the list when you have diagnostics
-		auto_close = false, -- automatically close the list when you have no diagnostics
+		auto_close = true, -- automatically close the list when you have no diagnostics
 		auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
 		auto_fold = false, -- automatically fold a file trouble list at creation
 		auto_jump = { "lsp_definitions" }, -- for the given modes, automatically jump if there is only a single result
