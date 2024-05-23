0f7db65093536d1ecc836879441320f2db861e64
Denis Cornehl <denis@cornehl.org>
2024-05-23 06:44:32 +0200

    vim: hide cursorline again, remove commented code

--- .config/nvim/lua/dc/plugins/focus.lua
+++ .config/nvim/lua/dc/plugins/focus.lua
@@ -5,8 +5,10 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("focus").setup({
-			cursorline = false,
-			signcolumn = false,
+			ui = {
+				cursorline = false,
+				signcolumn = false,
+			},
 			autoresize = {
 				enable = true,
 				minwidth = 60,
@@ -16,26 +18,9 @@ return {
 
 		-- disabling focus for certain filetypes:
 		-- https://github.com/nvim-focus/focus.nvim?tab=readme-ov-file#disabling-focus
-
 		local ignore_filetypes = { "fugitiveblame", "packer", "Trouble" }
-		-- local ignore_buftypes = { "nofile", "prompt", "popup" }
-
-		local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
-
-		-- vim.api.nvim_create_autocmd("WinEnter", {
-		-- 	group = augroup,
-		-- 	callback = function(_)
-		-- 		if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
-		-- 			vim.w.focus_disable = true
-		-- 		else
-		-- 			vim.w.focus_disable = false
-		-- 		end
-		-- 	end,
-		-- 	desc = "Disable focus autoresize for BufType",
-		-- })
-
 		vim.api.nvim_create_autocmd("FileType", {
-			group = augroup,
+			group = vim.api.nvim_create_augroup("FocusDisable", { clear = true }),
 			callback = function(_)
 				if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
 					vim.b.focus_disable = true
