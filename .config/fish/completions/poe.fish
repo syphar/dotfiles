a452493d1f972d1e87db8961675478ec74f0c588
Denis Cornehl <denis@cornehl.org>
2024-09-04 09:11:45 +0200

    fisher update

--- .config/fish/completions/poe.fish
+++ .config/fish/completions/poe.fish
@@ -1,6 +1,16 @@
 function __list_poe_tasks
+    # Check if `-C target_path` have been provided
+    set target_path ''
     set prev_args (commandline -pco)
-    set tasks (poe _list_tasks | string split " ")
+    for i in (seq (math (count $prev_args) - 1))
+        set j (math $i + 1)
+        set k (math $i + 2)
+        if test "$prev_args[$j]" = "-C" && test "$prev_args[$k]" != ""
+            set target_path "$prev_args[$k]"
+            break
+        end
+    end
+    set tasks (poe _list_tasks $target_path | string split ' ')
     set arg (commandline -ct)
     for task in $tasks
         if test "$task" != poe && contains $task $prev_args
