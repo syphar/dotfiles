78e3045415575fae0d650f04656c7044edb07577
Denis Cornehl <denis@cornehl.org>
2024-10-04 11:08:33 +0200

    fish: fix upstream-fetch function

--- .config/fish/functions/gffu.fish
+++ .config/fish/functions/gffu.fish
@@ -1,8 +1,8 @@
 # fetch from upstream/main&master
-function gcm
-    if git show-ref --verify --quiet refs/heads/master
+function gffu
+    if git show-ref --verify --quiet refs/remotes/upstream/master
         git ff upstream/master
-    else if git show-ref --verify --quiet refs/heads/main
+    else if git show-ref --verify --quiet refs/remotes/upstream/main
         git ff upstream/main
     else
         echo "Neither master nor main branch exists."
