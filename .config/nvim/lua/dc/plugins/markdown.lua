9baf8fe770d22b519062bb6d572ec29ed884e4da
Denis Cornehl <denis@cornehl.org>
2025-01-12 15:13:03 +0100

    vim: revert markdown options

--- .config/nvim/lua/dc/plugins/markdown.lua
+++ .config/nvim/lua/dc/plugins/markdown.lua
@@ -1,15 +1,5 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	opts = {
-		code = {
-			sign = false,
-			width = "block",
-			right_pad = 1,
-		},
-		heading = {
-			sign = false,
-			icons = {},
-		},
-	},
+	opts = {},
 }
