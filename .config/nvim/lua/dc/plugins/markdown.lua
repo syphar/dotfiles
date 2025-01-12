94126784aad5df1a7af1887e89775f187cb40b74
Denis Cornehl <denis@cornehl.org>
2025-01-12 14:25:02 +0100

    vim: try markdown config

--- .config/nvim/lua/dc/plugins/markdown.lua
+++ .config/nvim/lua/dc/plugins/markdown.lua
@@ -1,5 +1,15 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		code = {
+			sign = false,
+			width = "block",
+			right_pad = 1,
+		},
+		heading = {
+			sign = false,
+			icons = {},
+		},
+	},
 }
