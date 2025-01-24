fb67a2788a5ded6d978ded2e6f5ded4047ef3437
Denis Cornehl <denis@cornehl.org>
2025-01-24 20:17:20 +0100

    vim: update tsserver setup

--- .config/nvim/lua/dc/lsp/tsserver.lua
+++ .config/nvim/lua/dc/lsp/tsserver.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup(cfg, lspconfig)
-	lspconfig.tsserver.setup({
+	lspconfig.ts_ls.setup({
 		flags = cfg.global_flags(),
 		capabilities = cfg.capabilities(),
 		on_attach = cfg.lsp_on_attach,
