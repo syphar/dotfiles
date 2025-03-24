b46be82afd777de84660b3b4acbfc85c4af56b3f
Denis Cornehl <denis@cornehl.org>
2025-03-24 10:15:49 +0100

    fish: alias for bacon clippy

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -50,6 +50,8 @@ abbr -ag h heroku
 abbr -ag hl heroku login
 alias heroku-shell "heroku run \"./manage.py shell\""
 
+abbr -ag bc bacon clippy
+
 alias ll="eza --long --all --header --icons --group-directories-first --color-scale=size --time-style=relative --git"
 alias l="eza --icons --group-directories-first"
 alias top="ytop"
