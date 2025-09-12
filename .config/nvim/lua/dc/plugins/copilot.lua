0ed1e4c87f1fc7dc928ee6f092dc98e296b10e4c
Denis Cornehl <denis@cornehl.org>
2025-09-12 07:28:02 +0200

    switch to chatgpt5 for codecompanion

--- .config/nvim/lua/dc/plugins/copilot.lua
+++ .config/nvim/lua/dc/plugins/copilot.lua
@@ -13,7 +13,30 @@ return {
 	{
 		"olimorris/codecompanion.nvim",
 		lazy = true,
-		opts = {},
+		opts = {
+			strategies = {
+				chat = {
+					adapter = "openai",
+				},
+				inline = {
+					adapter = "openai",
+				},
+				cmd = {
+					adapter = "openai",
+				},
+			},
+			adapters = {
+				openai = function()
+					return require("codecompanion.adapters").extend("openai", {
+						schema = {
+							model = {
+								default = "gpt-5",
+							},
+						},
+					})
+				end,
+			},
+		},
 		cmd = {
 			"CodeCompanionChat",
 			"CodeCompanion",
