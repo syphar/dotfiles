8091dab89e6b924ef95012af51181c1188f0bc2e
Denis Cornehl <denis@cornehl.org>
2025-12-18 06:31:00 +0100

    vim: update copilot model

--- .config/nvim/lua/dc/plugins/copilot.lua
+++ .config/nvim/lua/dc/plugins/copilot.lua
@@ -13,7 +13,7 @@ return {
 		cmd = "Copilot",
 		event = "InsertEnter",
 		opts = {
-			copilot_model = "gpt-4o-copilot",
+			copilot_model = "gpt-41-copilot",
 			suggestion = { enabled = true, auto_trigger = true, debounce = 500 },
 			panel = { enabled = false },
 			copilot_node_command = "/opt/homebrew/bin/node",
