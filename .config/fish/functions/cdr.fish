683a5c5a2c802083cf1f554cc73a1f1dccb2f4f7
Denis Cornehl <denis@cornehl.org>
2026-01-05 08:30:29 +0100

    fish: safer `cdr` function

--- .config/fish/functions/cdr.fish
+++ .config/fish/functions/cdr.fish
@@ -1,7 +1,6 @@
-# jump to the root of the current git repository
 function cdr
     set -l root (git rev-parse --show-toplevel 2>/dev/null)
     if test -n "$root"
-        cd $root
+        cd "$root"
     end
 end
