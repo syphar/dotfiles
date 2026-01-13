e7e2d35a3a6e5dc1443c35d529db28a5487792a3
Denis Cornehl <denis@cornehl.org>
2026-01-13 07:36:03 +0100

    vim: add asyncapi schema to yamlls

--- .config/nvim/lua/dc/lsp/yamlls.lua
+++ .config/nvim/lua/dc/lsp/yamlls.lua
@@ -7,10 +7,15 @@ function M.config(cfg)
 		on_attach = cfg.lsp_on_attach,
 		settings = {
 			yaml = {
+				schemaStore = {
+					enable = true,
+				},
 				schemas = {
 					["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "/docker-compose.yml",
+					["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+					"/docker-compose.yml",
 					["https://json.schemastore.org/dependabot-2.0.json"] = "/.github/dependabot.yml",
+					["https://www.asyncapi.com/schema-store/all.schema-store.json"] = "asyncapi.yml",
 				},
 			},
 		},
