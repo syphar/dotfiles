0af0954623af7c54229891eb9f1b820ae247c109
Denis Cornehl <denis@cornehl.org>
2026-03-10 07:22:42 +0100

    vim: git status in file manage

--- .config/nvim/lua/dc/plugins/filemanager.lua
+++ .config/nvim/lua/dc/plugins/filemanager.lua
@@ -1,17 +1,30 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {
-		keymaps = {
-			["q"] = { "actions.close", mode = "n" },
-			["<ESC>"] = { "actions.close", mode = "n" },
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			keymaps = {
+				["q"] = { "actions.close", mode = "n" },
+				["<ESC>"] = { "actions.close", mode = "n" },
+			},
+			win_options = {
+				signcolumn = "yes:1",
+			},
 		},
-	},
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-	keys = {
-		{
-			"-",
-			"<CMD>Oil --float<CR>",
-			{ desc = "Open parent directory" },
+		dependencies = {
+			{ "echasnovski/mini.icons", opts = {} },
+			"nvim-tree/nvim-web-devicons", -- use if prefer nvim-web-devicons
+			{
+				"malewicz1337/oil-git.nvim",
+				dependencies = { "stevearc/oil.nvim" },
+				opts = {},
+			},
+		},
+		keys = {
+			{
+				"-",
+				"<CMD>Oil --float<CR>",
+				{ desc = "Open parent directory" },
+			},
 		},
 	},
 }
