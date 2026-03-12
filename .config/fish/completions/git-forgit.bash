bb828357b282f3a123b1061ebd5707491058e756
Denis Cornehl <denis@cornehl.org>
2026-03-12 08:56:46 +0100

    fisher update

--- .config/fish/completions/git-forgit.bash
+++ .config/fish/completions/git-forgit.bash
@@ -67,6 +67,7 @@ _git_forgit()
 
     cmds="
         add
+        attributes
         blame
         branch_delete
         checkout_branch
@@ -89,6 +90,7 @@ _git_forgit()
         squash
         stash_show
         stash_push
+        switch_branch
         worktree
         worktree_delete
     "
@@ -100,6 +102,8 @@ _git_forgit()
         3)
             case ${prev} in
                 add) _git_add ;;
+                attributes) ;;
+                blame) ;;
                 branch_delete) _git_branch_delete ;;
                 checkout_branch) _git_checkout_branch ;;
                 checkout_commit) _git_checkout ;;
@@ -110,6 +114,7 @@ _git_forgit()
                 clean) _git_clean ;;
                 diff) _git_diff ;;
                 fixup) _git_log ;;
+                ignore) ;;
                 log) _git_log ;;
                 reflog) _git_reflog ;;
                 rebase) _git_rebase ;;
@@ -119,6 +124,8 @@ _git_forgit()
                 show) _git_show ;;
                 squash) _git_log ;;
                 stash_show) _git_stash_show ;;
+                stash_push) _git_add ;;
+                switch_branch) _git_switch ;;
                 worktree) _git_worktree ;;
                 worktree_delete) _git_worktrees ;;
             esac
@@ -141,7 +148,7 @@ then
     __git_complete forgit::add _git_add
     __git_complete forgit::branch::delete _git_branch_delete
     __git_complete forgit::checkout::branch _git_checkout_branch
-    __git_complete forgit::switch::branch _git_switch_branch
+    __git_complete forgit::switch::branch _git_switch
     __git_complete forgit::checkout::commit _git_checkout
     __git_complete forgit::checkout::file _git_checkout_file
     __git_complete forgit::checkout::tag _git_checkout_tag
@@ -159,6 +166,7 @@ then
     __git_complete forgit::show _git_show
     __git_complete forgit::squash _git_log
     __git_complete forgit::stash::show _git_stash_show
+    __git_complete forgit::stash::push _git_add
     __git_complete forgit::worktree _git_worktree
     __git_complete forgit::worktree::delete _git_worktrees
 
@@ -167,7 +175,7 @@ then
         __git_complete "${forgit_add}" _git_add
         __git_complete "${forgit_branch_delete}" _git_branch_delete
         __git_complete "${forgit_checkout_branch}" _git_checkout_branch
-        __git_complete "${forgit_switch_branch}" _git_switch_branch
+        __git_complete "${forgit_switch_branch}" _git_switch
         __git_complete "${forgit_checkout_commit}" _git_checkout
         __git_complete "${forgit_checkout_file}" _git_checkout_file
         __git_complete "${forgit_checkout_tag}" _git_checkout_tag
@@ -184,6 +192,7 @@ then
         __git_complete "${forgit_show}" _git_show
         __git_complete "${forgit_squash}" _git_log
         __git_complete "${forgit_stash_show}" _git_stash_show
+        __git_complete "${forgit_stash_push}" _git_add
         __git_complete "${forgit_worktree}" _git_worktree
         __git_complete "${forgit_worktree_delete}" _git_worktrees
     fi
