b77f44042e1ab901a9acda23fbf9264136474cc8
Denis Cornehl <denis@cornehl.org>
2026-04-08 13:52:10 +0200

    vim: new git-conflict plugin

--- .config/nvim/lua/dc/plugins/git_conflict.lua
+++ .config/nvim/lua/dc/plugins/git_conflict.lua
@@ -1,33 +1,19 @@
 return {
-	"akinsho/git-conflict.nvim",
-	version = "*",
-	event = { "BufReadPost", "BufNewFile" },
-	config = function()
-		require("git-conflict").setup({
-			default_mappings = true,
-			disable_diagnostics = true,
-			highlights = { -- They must have background color, otherwise the default color will be used
-				incoming = "DiffText",
-				current = "DiffAdd",
-			},
-		})
-
-		-- from https://github.com/akinsho/git-conflict.nvim?tab=readme-ov-file#autocommands
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "GitConflictDetected",
-			callback = function()
-				vim.notify("Conflict detected in " .. vim.fn.expand("<afile>"))
-				vim.cmd([[GitConflictListQf]])
-				vim.cmd([[copen]])
-			end,
-		})
-	end,
+	"niekdomi/conflict.nvim",
+	opts = {
+		default_mappings = {
+			current = "cc",
+			incoming = "ci",
+			both = "cb",
+			none = false,
+			next = "]x",
+			prev = "[x",
+		},
+		show_actions = false,
+		disable_diagnostics = true,
+		highlights = {
+			incoming = "DiffAdd",
+			current = "DiffText",
+		},
+	},
 }
-
--- default mappings
--- vim.keymap.set("n", "co", "<Plug>(git-conflict-ours)")
--- vim.keymap.set("n", "cb", "<Plug>(git-conflict-both)")
--- vim.keymap.set("n", "c0", "<Plug>(git-conflict-none)")
--- vim.keymap.set("n", "ct", "<Plug>(git-conflict-theirs)")
--- vim.keymap.set("n", "[x", "<Plug>(git-conflict-next-conflict)")
--- vim.keymap.set("n", "]x", "<Plug>(git-conflict-prev-conflict)")
