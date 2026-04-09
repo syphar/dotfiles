7c7fc08483669e91ba5e5f2135f093349fe562e2
Denis Cornehl <denis@cornehl.org>
2026-04-09 11:31:37 +0200

    vim: try treesitter autoinstall hack

--- .config/nvim/lua/dc/plugins/treesitter.lua
+++ .config/nvim/lua/dc/plugins/treesitter.lua
@@ -2,14 +2,25 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "*",
 	callback = function(args)
 		local bufnr = args.buf
-		-- local filetype = vim.bo[bufnr].filetype
-		-- local lang = vim.treesitter.language.get_lang(filetype) or filetype
-
+		local filetype = vim.bo[bufnr].filetype
 		if vim.bo[bufnr].buftype ~= "" then
 			return
 		end
 
-		vim.treesitter.start()
+		local lang = vim.treesitter.language.get_lang(filetype)
+		if lang and vim.tbl_contains(require("nvim-treesitter").get_available(), lang) then
+			local installed = require("nvim-treesitter").get_installed("parsers")
+			if not vim.tbl_contains(installed, lang) then
+				require("nvim-treesitter").install(lang)
+				return
+			end
+		end
+
+		local ok = pcall(vim.treesitter.start)
+		if ok == false then
+			return
+		end
+
 		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 		vim.wo.foldmethod = "expr"
 		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
@@ -22,8 +33,6 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
-			auto_install = true,
-		})
+		require("nvim-treesitter").setup()
 	end,
 }
