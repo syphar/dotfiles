24536a3e002cc55ef62dcf2f06ebe634f981dca7
Denis Cornehl <denis@cornehl.org>
2026-04-19 20:14:26 +0200

    fish: standard way to find default branch

--- .config/fish/functions/_git_default_branch.fish
+++ .config/fish/functions/_git_default_branch.fish
@@ -1,10 +1,16 @@
-function _git_default_branch -d "Echo the default branch name (main or master)"
+function _git_default_branch -d "Echo the repo default branch name"
+    set -l remote_default (command git symbolic-ref --quiet --short refs/remotes/origin/HEAD 2>/dev/null)
+    if test -n "$remote_default"
+        echo (string replace 'origin/' '' -- "$remote_default")
+        return 0
+    end
+
     if git show-ref --verify --quiet refs/heads/main
         echo main
     else if git show-ref --verify --quiet refs/heads/master
         echo master
     else
-        echo "_git_default_branch: neither main nor master branch exists" >&2
+        echo "_git_default_branch: could not determine default branch" >&2
         return 1
     end
 end
