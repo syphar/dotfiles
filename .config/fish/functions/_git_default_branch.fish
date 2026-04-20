62ea57fd67d81b9cb4dc0395e752a50a5356ca06
Denis Cornehl <denis@cornehl.org>
2026-04-20 08:52:34 +0200

    fish: default branch detection: back to static

--- .config/fish/functions/_git_default_branch.fish
+++ .config/fish/functions/_git_default_branch.fish
@@ -1,10 +1,4 @@
 function _git_default_branch -d "Echo the repo default branch name"
-    set -l remote_default (command git symbolic-ref --quiet --short refs/remotes/origin/HEAD 2>/dev/null)
-    if test -n "$remote_default"
-        echo (string replace 'origin/' '' -- "$remote_default")
-        return 0
-    end
-
     if git show-ref --verify --quiet refs/heads/main
         echo main
     else if git show-ref --verify --quiet refs/heads/master
