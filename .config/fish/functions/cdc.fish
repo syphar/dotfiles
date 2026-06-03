75893a6ec64ac4f3650b7f5d928448fda7196814
Denis Cornehl <denis@cornehl.org>
2026-06-03 09:19:09 +0200

    fish / `cdc`, support python monorepos

--- .config/fish/functions/cdc.fish
+++ .config/fish/functions/cdc.fish
@@ -1,11 +1,11 @@
-# select a subcrate to jump to. searches from the repo root
+# select a subcrate/subproject to jump to. searches from the repo root
 function cdc
     set -l root (git rev-parse --show-toplevel 2>/dev/null)
     test -n "$root"; or return
 
     set -l target (
         fd \
-            --glob Cargo.toml \
+            --glob '{Cargo,pyproject}.toml' \
             --type f \
             --relative-path \
             --base-directory $root \
