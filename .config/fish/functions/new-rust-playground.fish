47769daaa8be6ca577394e2b5caf37fe064532ba
Denis Cornehl <denis@cornehl.org>
2026-09-01 08:22:37 +0200

    better new-playgroun

--- .config/fish/functions/new-rust-playground.fish
+++ .config/fish/functions/new-rust-playground.fish
@@ -1,11 +1,19 @@
 function new-rust-playground --description "Create a temporary Rust playground project"
     set tmp_dir (mktemp -d ~/tmp/rust-playground.XXXXXX)
-    
+
     set project_name "playground-"(random)
-    
-    cargo init --bin "$tmp_dir/$project_name"
-    
-    cd "$tmp_dir/$project_name"
-    
+    set project_dir "$tmp_dir/$project_name"
+
+    cargo init --bin "$project_dir"
+
+    printf '%s\n' \
+        'fn main() -> anyhow::Result<()> {' \
+        '    Ok(())' \
+        '}' >"$project_dir/src/main.rs"
+
+    cd "$project_dir"
+
     cargo add url anyhow
+    cargo check
+    cargo fmt
 end
